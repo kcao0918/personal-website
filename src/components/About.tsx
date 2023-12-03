b85b92dc5900101a6fda6d9071db9bf769008f93
Kenny <kcao0918@gmail.com>
2023-12-03 16:43:35 -0500

    updated menu

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,21 +4,29 @@ import styles from "./About.module.css";
 const About = () => {
   return (
     <div className={styles.AboutMe}>
-      I am currently in my sophomore year at Georgia Tech, majoring in computer
-      science with a focus on Intelligence and System Architecture. My
-      enthusiasm for Web Development and my passion for solving coding problems
-      have fueled my journey in programming. Proficient in Python, Java,
-      HTML/CSS, and TypeScript, I also leverage the React library for crafting
+      I am currently in my sophomore year at
+      <div className={styles.Highlight}> Georgia Tech</div>, majoring in
+      computer science with a focus on
+      <div className={styles.Highlight}> Intelligence</div> and
+      <div className={styles.Highlight}> System Architecture</div>. My
+      enthusiasm for <div className={styles.Highlight}> Web Development</div>{" "}
+      and my passion for solving coding problems have fueled my journey in
+      programming. Proficient in <div className={styles.Highlight}> Python</div>
+      ,<div className={styles.Highlight}> Java</div>,
+      <div className={styles.Highlight}> HTML/CSS/JS</div>, and
+      <div className={styles.Highlight}> TypeScript</div>, I also leverage the
+      <div className={styles.Highlight}> React</div> library for crafting
       dynamic and responsive web applications, such as the one you are currently
       exploring.
       <br />
       <br />
       When I'm not engrossed in classes or preparing for exams, I find joy in
-      the intricacies of chess, even though I wouldn't call myself a grandmaster
-      just yet. Additionally, I immerse myself in the captivating realms of
-      science fiction novels. Currently, my literary fascination is 'Three Body
-      Problem' by Cixin Liu, an enthralling sci-fi narrative that unfolds across
-      millennia.
+      the intricacies of <div className={styles.Highlight}>chess</div>, even
+      though I wouldn't call myself a grandmaster just yet. Additionally, I
+      immerse myself in the captivating realms of
+      <div className={styles.Highlight}> science fiction works</div>. Currently,
+      my literary fascination is 'Three Body Problem' by Cixin Liu, an
+      enthralling sci-fi narrative that unfolds across millennia.
     </div>
   );
 };
--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -3,8 +3,20 @@ import githublogo from "../assets/images/githublogo.png";
 import linkedinlogo from "../assets/images/linkedinlogo.png";
 import emaillogo from "../assets/images/emaillogo.png";
 import { Link } from "react-scroll";
+import { useState } from "react";
 
 const Overview = () => {
+  const [activeSection, setActiveSection] = useState("About");
+
+  const handleActiveSection = (to: string) => {
+    setActiveSection(to);
+  };
+
+  const handleDashes = (sect: string) => {
+    const isActive = activeSection === sect;
+    return isActive ? `———— ${sect}` : `—— ${sect}`;
+  };
+
   return (
     <div className={styles["Overview"]}>
       <div className={styles.Name}>Kenny Cao</div>
@@ -15,34 +27,37 @@ const Overview = () => {
 
       <div className={styles.Menu}>
         <Link
-          activeStyle={{ color: "#72d4d6" }}
           to="about"
           spy={true}
           smooth={true}
           duration={500}
           className={styles.AboutSelect}
+          onSetActive={() => handleActiveSection("About")}
+          style={{ color: activeSection === "About" ? "#72d4d6" : "#d3d6e6" }}
         >
-          About
+          {handleDashes("About")}
         </Link>
         <Link
-          activeStyle={{ color: "#72d4d6" }}
           to="experiences"
           spy={true}
           smooth={true}
           duration={500}
           className={styles.ExpSelect}
+          onSetActive={() => handleActiveSection("Experiences")}
+          style={{ color: activeSection === "Experiences" ? "#72d4d6" : "#d3d6e6" }}
         >
-          Experiences
+          {handleDashes("Experiences")}
         </Link>
         <Link
-          activeStyle={{ color: "#72d4d6" }}
           to="projects"
           spy={true}
           smooth={true}
           duration={500}
           className={styles.ProjectsSelect}
+          onSetActive={() => handleActiveSection("Projects")}
+          style={{ color: activeSection === "Projects" ? "#72d4d6" : "#d3d6e6" }}
         >
-          Projects
+          {handleDashes("Projects")}
         </Link>
       </div>
 
