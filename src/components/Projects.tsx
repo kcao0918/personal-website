bf19debfbf8f270145adb356d65417072fe73290
Kenny <kcao0918@gmail.com>
2023-12-06 17:18:48 -0500

    fixed formatting and worked on mobile media query

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Projects.module.css";
 
 const Projects = () => {
-  return <div className={styles.ProjectContainer}>Projects</div>;
+  return <div className={styles.ProjectContainer}>Projects Coming Soon</div>;
 };
 
 export default Projects;
