08f6774fd8f4e2e01c860d5a53572232830a017d
kcao0918 <kcao0918@gmail.com>
2024-01-30 11:09:06 -0500

    updated experiences page

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -2,58 +2,79 @@ import React from "react";
 import styles from "./Experiences.module.css";
 
 const Experiences = () => {
-  const experiences = [
-    {
-      date: "JUL 22 - AUG 23",
-      position: "Teacher/Tutor · Ivy Road Prep",
-      description:
-        "Instructed students in subjects including computer science, math, english. Actively contributed to the learning process by creating comprehensive lesson plans, designing assessments, and facilitating engaging classroom experiences. Notably, I observed significant improvements in their grades, highlighting the positive impact of my instruction.",
-      techstack: ["Python", "Java"],
-    },
-    {
-      date: "SEP 21 - JUN 22",
-      position: "App Developer",
-      description:
-        "Utilized app development tools, including Flutter and Android Studio. Acquired proficiency in Dart and Java programming languages to design functional user interfaces. Notable projects include a cold calling application and the dynamic app Gide, enabling shops to register and manage discounts. Users can seamlessly input their location and discover nearby shops, enhancing convenience and accessibility.",
-      techstack: ["Android Studio", "Java", "Flutter", "Dart", "Firebase"],
-    },
-    {
-      date: "OCT 20 - MAY 21",
-      position:
-        "Participant · Qubit by Qubit: Introduction to Quantum Computing",
-      description:
-        "Qubit by Qubit participant, engaging in quantum computing education programs. Acquired foundational knowledge and practical skills in quantum computing principles. Collaborated on hands-on activities and coding exercises to advance proficiency in this emerging field.",
-      techstack: ["Python"],
-    },
-    {
-      date: "JUL 21 - AUG 21",
-      position: "Office Assistant · SONYC",
-      description:
-        "Operated the campus bazaar, streamlining student point exchanges for items and organizing engaging classroom events. Assisted in daily document preparation and filing for efficient office management.",
-      techstack: ["Google Suites"],
-    },
-  ];
-  return (
-    <div className={styles.ExpContainer}>
-      {experiences.map((experience) => (
-        <div className={styles.Item}>
-          <div className={styles.ItemDate}>{experience.date}</div>
-          <div className={styles.ItemBody}>
-            <div className={styles.ItemPosition}>{experience.position}</div>
-            <br />
-            {experience.description}
-            <div className={styles.TechList}>
-              {experience.techstack.map((tech, index) => (
-                <div className={styles.TechStack} key={index}>
-                  {tech}
+    const experiences = [
+        {
+            date: "JAN 24 - PRESENT",
+            position: "Fullstack Developer · GT WebDev",
+            description: "Creation of a collaborative whiteboard application using both front-end and back-end frameworks",
+            techstack: ["HTML", "CSS", "JS", "MongoDB"],
+        },
+        {
+            date: "JAN 24 - PRESENT",
+            position:
+                "Undergraduate Researcher · Georgia Tech Financial Services and Innovation Lab",
+            description: "Training a classifier using a dataset aimed at organizing news headlines by topic. Implementing a method to automatically discern financially relevant news by utilizing stock price movements and news headlines",
+            techstack: [],
+        },
+        {
+            date: "JUL 22 - AUG 23",
+            position: "Teacher/Tutor · Ivy Road Prep",
+            description:
+                "Instructed students in subjects including computer science, math, english. Actively contributed to the learning process by creating comprehensive lesson plans, designing assessments, and facilitating engaging classroom experiences. Notably, I observed significant improvements in their grades, highlighting the positive impact of my instruction.",
+            techstack: ["Python", "Java"],
+        },
+        {
+            date: "SEP 21 - JUN 22",
+            position: "App Developer",
+            description:
+                "Utilized app development tools, including Flutter and Android Studio. Acquired proficiency in Dart and Java programming languages to design functional user interfaces. Notable projects include a cold calling application and the dynamic app Gide, enabling shops to register and manage discounts. Users can seamlessly input their location and discover nearby shops, enhancing convenience and accessibility.",
+            techstack: [
+                "Android Studio",
+                "Java",
+                "Flutter",
+                "Dart",
+                "Firebase",
+            ],
+        },
+        {
+            date: "OCT 20 - MAY 21",
+            position:
+                "Participant · Qubit by Qubit: Introduction to Quantum Computing",
+            description:
+                "Qubit by Qubit participant, engaging in quantum computing education programs. Acquired foundational knowledge and practical skills in quantum computing principles. Collaborated on hands-on activities and coding exercises to advance proficiency in this emerging field.",
+            techstack: ["Python"],
+        },
+        {
+            date: "JUL 21 - AUG 21",
+            position: "Office Assistant · SONYC",
+            description:
+                "Operated the campus bazaar, streamlining student point exchanges for items and organizing engaging classroom events. Assisted in daily document preparation and filing for efficient office management.",
+            techstack: ["Google Suites"],
+        },
+    ];
+    return (
+        <div className={styles.ExpContainer}>
+            {experiences.map((experience) => (
+                <div className={styles.Item}>
+                    <div className={styles.ItemDate}>{experience.date}</div>
+                    <div className={styles.ItemBody}>
+                        <div className={styles.ItemPosition}>
+                            {experience.position}
+                        </div>
+                        <br />
+                        {experience.description}
+                        <div className={styles.TechList}>
+                            {experience.techstack.map((tech, index) => (
+                                <div className={styles.TechStack} key={index}>
+                                    {tech}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-              ))}
-            </div>
-          </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Experiences;
